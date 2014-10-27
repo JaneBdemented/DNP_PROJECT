ce043d51c3b665a1f2345840faa308f9225d6418
JaneBdemented <jacqueline.waitman@gmail.com>
2014-10-27 15:14:10 -0400

    Update DNP_tables.php

--- DNP_tables.php
+++ DNP_tables.php
@@ -24,43 +24,43 @@ $conn = new mysqli($servername, $username, $password, $DNPDB);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "CREATE TABLE Stream (stream_id tinyint(4) PRIMARY KEY, stream TEXT)";
+$sql = "CREATE TABLE Stream (stream_id int(4) PRIMARY KEY, stream varchar(8))";
 if ($conn->query($sql) === TRUE) {
     echo "TABLE stream created successfully";
 } else {
     echo "Error creating TABLE: " . $conn->error;
 }
-$sql = "CREATE TABLE Courses (course_id smallint primary key NOT NULL, 
-							  subject char(4) NOT NULL, 
-							  courseNUM smallint NOT NULL, 
-							  name text, 
-							  semester bit NOT NULL,
-							  has_lab bit, 
-							  has_tut bit)";
+$sql = "CREATE TABLE Courses (course_id int(4) primary key NOT NULL, 
+							  SUBJ varchar(4) NOT NULL, 
+							  CRSE_NUM int(4) NOT NULL, 
+							  NAME text, 
+							  SMSTR int(1) NOT NULL,
+							  HAS_LAB varchar(1), 
+							  HAS_TUT varchar(1))";
 if ($conn->query($sql) === TRUE) {
     echo "TABLE Couses created successfully";
 } else {
     echo "Error creating TABLE: " . $conn->error;
 }
-$sql = "CREATE TABLE Course_Timing (CoursTime_id smallint primary key NOT NULL, 
-									course_id smallint, 
-									type char(3), 
-									time_s smallint, 
-									time_e int, 
-									room smallint, 
-									capacity smallint, 
-									has_prereq bit, 
-									has_yearStanding tinyint(4),
+$sql = "CREATE TABLE Course_Timing (CoursTime_id int(4) primary key NOT NULL, 
+									course_id int(4), 
+									SUB_VAL varchar(3),
+									TYPE varcher(3)
+									time_s varchar(4), 
+									time_e varchar(4), 
+									ROOM_CAP varchar(3), 
+									has_prereq int(1), 
+									has_yearStanding int(1),
 									FOREIGN KEY (course_id) REFERENCES Courses(course_id) )";
 if ($conn->query($sql) === TRUE) {
     echo "TABLE Couses_timing created successfully";
 } else {
     echo "Error creating TABLE: " . $conn->error;
 }
-$sql = "CREATE TABLE Stream_Courses (StremCours_id smallint primary key NOT NULL, 
-									 stream_id tinyint(4) NOT NULL, 
-									 course_id smallint NOT NULL, 
-									 year tinyint(4),
+$sql = "CREATE TABLE Stream_Courses (Stream_Courses_id smallint primary key NOT NULL, 
+									 stream_id int(4) NOT NULL, 
+									 course_id int(1) NOT NULL, 
+									 YEAR tinyint(1),
 									 FOREIGN KEY (stream_id) REFERENCES Stream(stream_id),
 									 FOREIGN KEY (course_id) REFERENCES Course_Timing(course_id))";
 if ($conn->query($sql) === TRUE) {
