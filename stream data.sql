667c052d6e3d905f52fd15e4fe3e08a1c2a6a9dc
JaneBdemented <jacqueline.waitman@gmail.com>
2014-10-27 15:29:11 -0400

    Create stream data.sql
     highlight the Stream in the DNP_PROJECT database and select sql tab, and copy and past the above code.

---
+++ stream data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Stream (stream_id,stream) VALUES (1,'Comms');
+	INSERT INTO Stream (stream_id,stream) VALUES (2,'BIO-ELEC');
+	INSERT INTO Stream (stream_id,stream) VALUES (3,'SE');
+	INSERT INTO Stream (stream_id,stream) VALUES (4,'CSE');
+	INSERT INTO Stream (Stream_id,stream) VALUES (5,'all');
